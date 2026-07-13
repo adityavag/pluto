7d58674bc87e6d0d2d2a3a657cda10ae06ab1b8f
Aditya <adi247u@gmail.com>
2026-07-13 19:10:06 +1000

    refactor: update request mapping path in AuthController from /api/account to /account

--- services/auth-service/src/main/java/com/pluto/authservice/controller/AuthController.java
+++ services/auth-service/src/main/java/com/pluto/authservice/controller/AuthController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/account")
+@RequestMapping("/account")
 @RequiredArgsConstructor
 public class AuthController {
 
